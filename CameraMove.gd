f0a6a8bc3e81290e422f4ddfab3e923fefd0691f
JalvinGaming <hellofellowdogz@gmail.com>
2024-03-19 09:52:49 +0000

    added roller coaster

--- CameraMove.gd
+++ CameraMove.gd
@@ -1,10 +1,16 @@
 extends Camera2D
 var MousePos = Vector2(0, 0)
 var oldMousePos = Vector2(0, 0)
+var ZoomIn = 0.5
+var ZoomOut = 0.5
 var Speed = Vector2(0, 0)
 func _process(delta):
 	MousePos = get_viewport().get_mouse_position()
 	Speed = Vector2(MousePos - oldMousePos)
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
-		translate(-Speed)
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE) or Input.is_action_pressed("moveable"):
+		translate(-Speed / zoom)
 	oldMousePos = get_viewport().get_mouse_position()
+	if Input.is_action_pressed("zoomin"):
+		zoom += Vector2(ZoomIn, ZoomIn) * zoom * delta
+	if Input.is_action_pressed("zoomout"):
+		zoom -= Vector2(ZoomOut, ZoomOut) * zoom * delta
--- MainScript.gd
+++ MainScript.gd
@@ -9,12 +9,14 @@ var SelectedCol
 var CursorCol
 var IsHolding = false
 var trueMousePos = Vector2(0, 0)
+var Camerabod
 
 func _ready():
 	CursorCol = get_node("Area2D")
+	Camerabod = get_node("Camera2D")
 
 func _process(delta):
-	trueMousePos = get_node("Camera2D").MousePos + get_node("Camera2D").position + get_viewport_rect().size * -0.5
+	trueMousePos = (Camerabod.MousePos / Camerabod.zoom) + Camerabod.position + (get_viewport_rect().size * -0.5 / Camerabod.zoom)
 	#Create Lines
 	if Mode == 1 and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and not IsHolding:
 		var newbody = Area2D.new()
@@ -22,25 +24,47 @@ func _process(delta):
 		var newpath = Line2D.new()
 		SelectedArea = newbody
 		SelectedArea.add_child(newpath)
+		#styling path
 		SelectedPath = newpath
+		SelectedPath.antialiased = true
+		SelectedPath.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
 		SelectedPath.add_point(trueMousePos)
+		SelectedPath.round_precision = 4
+		#add collision for selection
 		var newcol = CollisionPolygon2D.new()
 		SelectedArea.add_child(newcol)
 		SelectedCol = newcol
 		SelectedCol.polygon = [SelectedPath.points[0]]
-		SelectedCol.one_way_collision_margin = 20
+		SelectedCol.one_way_collision_margin = 14
 		#set styles
 		if true:
-			#set road style
+			#set roadstandard1lane style
 			if SelectedType == "RoadStandard1Lane":
 				SelectedPath.width = 8
 				SelectedPath.texture = load("res://Graphics/Lines/Roads/Standard 1 Lane/image.png")
 				SelectedPath.z_index = 1
 				SelectedPath.texture_mode = Line2D.LINE_TEXTURE_STRETCH
 				SelectedPath.texture_repeat = CanvasItem.TEXTURE_REPEAT_DISABLED
+				SelectedPath.begin_cap_mode = Line2D.LINE_CAP_ROUND
+				SelectedPath.end_cap_mode = Line2D.LINE_CAP_ROUND
+				SelectedPath.joint_mode = Line2D.LINE_JOINT_ROUND
+			#set rollercoasterwooden style
+			elif SelectedType == "RollerCoasterWooden":
+				SelectedPath.width = 5
+				SelectedPath.texture = load("res://Graphics/Lines/Amusements/RollerCoasterWooden/image.png")
+				SelectedPath.z_index = 3
+				SelectedPath.texture_mode = Line2D.LINE_TEXTURE_TILE
+				SelectedPath.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+				SelectedPath.joint_mode = Line2D.LINE_JOINT_ROUND
+			#set land style
+			elif SelectedType == "Land":
+				SelectedPath.default_color = Color(0, 0.3, 0, 1)
+				SelectedPath.width = 2
+				SelectedPath.closed = true
 			#set default style
 			else:
 				SelectedPath.z_index = 1
+				SelectedPath.width = 6
 		
 		#Create poly
 		var newpoly = Polygon2D.new()
@@ -49,9 +73,15 @@ func _process(delta):
 		
 		#set styles
 		if true:
-			#set road style
+			#set roadstandard1lane style
 			if SelectedType == "RoadStandard1Lane":
 				SelectedPoly.color = Color(0, 0, 0, 0)
+			#set rollercoasterwooden style
+			elif SelectedType == "RollerCoasterWooden":
+				SelectedPoly.color = Color(0, 0, 0, 0)
+			#set land style
+			elif SelectedType == "Land":
+				SelectedPoly.color = Color(0.2, 0.8, 0.3, 1)
 			#set default style
 			else:
 				SelectedPoly.color = Color(1, 1, 1, 0.5)
@@ -60,7 +90,7 @@ func _process(delta):
 		#Create node
 		var newnode = Sprite2D.new()
 		newnode.texture = load("res://Graphics/Aids/Node.svg")
-		newnode.scale = Vector2(0.1, 0.1)
+		newnode.scale = Vector2(0.05, 0.05)
 		newnode.centered = true
 		newnode.position = trueMousePos
 		SelectedArea.add_child(newnode)
@@ -79,11 +109,13 @@ func _process(delta):
 		IsHolding = true
 		if CursorCol.get_overlapping_bodies().size() != 0:
 			SelectedArea = CursorCol.get_overlapping_bodies()[0]
+			print_debug(SelectedArea.name)
 			SelectedPath = SelectedArea.get_child(0)
 			SelectedCol = SelectedArea.get_child(1)
 			SelectedPoly = SelectedArea.get_child(2)
-			if SelectedArea.get_child(2) != null:
+			if SelectedArea.get_child(3) != null:
 				SelectedNode = SelectedArea.get_child(3)
+			
 	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		IsHolding = false
 #Handle Buttons
